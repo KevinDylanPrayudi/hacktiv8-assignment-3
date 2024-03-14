a9977d796c6afc334dee8c972e02e0c082215357
KevinDylanPrayudi <70803233+KevinDylanPrayudi@users.noreply.github.com>
2024-03-14 10:14:56 +0700

    create git ignore & add init func

--- main.go
+++ main.go
@@ -3,7 +3,18 @@ package main
 import (
 	"assignment-3/helpers"
 	"assignment-3/routers"
+	"log"
+	"os"
 )
+
+func init() {
+	cwd, _ := os.Getwd()
+	_, err := os.ReadFile(cwd + "\\test.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("test.json exist")
+}
 
 func main() {
 	go helpers.WriteJson()
